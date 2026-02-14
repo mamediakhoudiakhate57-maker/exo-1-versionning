03302194b162fce84c37ae5b26275abf471b43bc
mamediakhoudiakhate <mamediakhoudiakhate57@gmail.com>
2026-02-14 22:45:32 +0000

    moyenne impair

--- exo1 versionning.c
+++ exo1 versionning.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
 int main(){
-    int i,x,n;
+    int i,x,n,cpt=0,somme=0,nbr;
+    float moy;
     do{
      printf("saisir n entiers:");
     scanf("%d", &n);
     }while(n<0);
-      for(i=0;i<n;i++){
+      for(i=1;i<n;i++){
             do{
                  printf("veullez saisr un entier negatif:");
         scanf("%d", &x);
             }while(x>0);
             if(x%2==0){
                 somme+=x;
-                cpt=++;
+                cpt ++ ;
             }
 
-
       }
-      moy=som/cpt;
-      printf("la moyenne des nomnre pair est :%f",moy);
+      moy=somme/cpt;
+      printf("la moyenne pair est :%f",moy);
       nbr=nbr+x;
-      printf("le nombre des est :%d",nbr);
-
-
-
+      printf("le nombre des,pair est :%d",nbr)
+      for(i=1;i<=n;i++){
+            do{
+                 printf("veullez saisr un entier negatif:");
+        scanf("%d", &x);
+            }while(x>0);
+        if(i % 2 != 0) {
+            somme+=x;
+        }
+      }
+ printf("La moyenne impair est : %f", moy);
+   nbr=nbr+x;
+      printf("le nombre des impair est:%d",nbr)
 return 0;
 }
